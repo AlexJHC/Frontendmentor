fa8b281a4dd85b8d6765b4ab957f4977a711aee5
TheAlex01 <82757753+TheAlex01@users.noreply.github.com>
2021-06-20 16:18:42 +0300

    article preview readme & js changes

--- article-preview-component-master/script.js
+++ article-preview-component-master/script.js
@@ -1,20 +1,32 @@
-document.querySelector('.btn-emu').addEventListener("click", () => {
-  document.querySelector('.social').classList.toggle('hide')
+// Variables
+let button = document.querySelector('.btn-emu')
+let socilMenu = document.querySelector('.social')
+
+// Toggle social menu
+button.addEventListener("click", () => {
+  socilMenu.classList.toggle('hide')
   return false
 })
 
-document.querySelector('body').addEventListener("dblclick", () => {
-  return false
-})
-
-document.querySelector('.btn-emu').addEventListener("mouseenter", () => {
+// Fill collor while social button, white mouse pointer inside 
+button.addEventListener("mouseenter", () => {
   document.querySelector('#path2').style.fill = "white"
   return false
 })
 
-document.querySelector('.btn-emu').addEventListener("mouseleave", () => {
+// Return color to social button, while mouse pointer live 
+button.addEventListener("mouseleave", () => {
   document.querySelector('#path2').style.fill = "#6E8098"
   return false
 })
 
-
+// Close social menu if mouse click outside menu or button while menu show
+window.onclick = (e) => {
+  if (!e.target.matches('div.btn-emu') 
+  && !e.target.matches('svg')
+  && !e.target.matches('path')
+  && !e.target.matches('.social')
+  && !socilMenu.classList.contains('hide')) {
+    socilMenu.classList.add('hide')
+  }
+}
